a0c4b6c197486529d48f9182cb86a4ce3888f7bc
rafaelturse <rafaelturse@hotmail.com>
2021-03-15 21:18:58 -0400

    hr-payroll added

--- workspace/hr-worker/src/main/java/com/turse/hrworker/entities/Worker.java
+++ workspace/hr-worker/src/main/java/com/turse/hrworker/entities/Worker.java
@@ -9,9 +9,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Data;
-
-@Data
 @Entity
 @Table(name = "tb_worker")
 public class Worker implements Serializable {
@@ -20,9 +17,36 @@ public class Worker implements Serializable {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
 	private Long id;
+
+	@Column(name = "NAME")
 	private String name;
 
-	@Column(name = "daily_Income")
+	@Column(name = "DAILY_INCOME")
 	private Double daylyIncome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getDaylyIncome() {
+		return daylyIncome;
+	}
+
+	public void setDaylyIncome(Double daylyIncome) {
+		this.daylyIncome = daylyIncome;
+	}
 }
