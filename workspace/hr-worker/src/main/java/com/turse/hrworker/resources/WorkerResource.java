634cf4153569a59863947fe3b8e6001be7326afb
rafaelturse <rafaelturse@hotmail.com>
2021-07-27 12:30:02 -0400

    ribbon load balance

--- workspace/hr-worker/src/main/java/com/turse/hrworker/resources/WorkerResource.java
+++ workspace/hr-worker/src/main/java/com/turse/hrworker/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package com.turse.hrworker.resources;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +17,12 @@ import com.turse.hrworker.repositories.WorkerRepository;
 
 @RestController
 @RequestMapping(value = "/workers")
-public class WorkResource {
+public class WorkerResource {
+
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+	@Autowired
+	private Environment env;
 
 	@Autowired
 	private WorkerRepository repository;
@@ -26,6 +34,8 @@ public class WorkResource {
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+		logger.info("PORT = " + env.getProperty("local.server.port"));
+
 		return ResponseEntity.ok(repository.findById(id).get());
 	}
 }
