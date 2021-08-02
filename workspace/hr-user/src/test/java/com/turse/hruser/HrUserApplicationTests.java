8e9898fb47f2b2fd0ebb71254c75b9a167bae661
rafaelturse <rafaelturse@hotmail.com>
2021-08-01 20:18:30 -0400

    hr-user run

---
+++ workspace/hr-user/src/test/java/com/turse/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turse.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
