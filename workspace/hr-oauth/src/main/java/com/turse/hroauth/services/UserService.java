97e4b6455055bede4d93309c320c2bf71662b829
rafaelturse <rafaelturse@hotmail.com>
2021-08-03 14:29:45 -0400

    hr-oauth communication with hr-user

---
+++ workspace/hr-oauth/src/main/java/com/turse/hroauth/services/UserService.java
@@ -0,0 +1,31 @@
+package com.turse.hroauth.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.turse.hroauth.entities.User;
+import com.turse.hroauth.feignclients.UserFeignClient;
+
+@Service
+public class UserService {
+
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+
+	@Autowired
+	private UserFeignClient userFieignClient;
+
+	public User findByEmail(String email) {
+		User user = userFieignClient.findByEmail(email).getBody();
+
+		if (user == null) {
+			logger.error("Email not found: " + email);
+			throw new IllegalArgumentException("Email not found");
+		}
+
+		logger.info("Email found: " + email);
+
+		return user;
+	}
+}
