979f3e882d199c26179ea2b57bf7c91d45e70225
rafaelturse <rafaelturse@hotmail.com>
2021-08-09 14:14:07 -0400

    spring security and JWT Token config

--- workspace/hr-oauth/src/main/java/com/turse/hroauth/services/UserService.java
+++ workspace/hr-oauth/src/main/java/com/turse/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package com.turse.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.turse.hroauth.entities.User;
 import com.turse.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -25,6 +28,21 @@ public class UserService {
 		}
 
 		logger.info("Email found: " + email);
+
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username)
+			throws UsernameNotFoundException {
+		User user = userFieignClient.findByEmail(username).getBody();
+
+		if (user == null) {
+			logger.error("Email not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+
+		logger.info("Email found: " + username);
 
 		return user;
 	}
