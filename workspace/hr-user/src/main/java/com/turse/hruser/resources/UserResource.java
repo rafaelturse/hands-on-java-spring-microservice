856ec58b0ca59908c07e6feec68a2782491391ac
rafaelturse <rafaelturse@hotmail.com>
2021-08-24 12:37:23 -0400

    db config move to git

--- workspace/hr-user/src/main/java/com/turse/hruser/resources/UserResource.java
+++ workspace/hr-user/src/main/java/com/turse/hruser/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.turse.hruser.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.turse.hruser.entities.User;
 import com.turse.hruser.repositories.UserRepositoriy;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
